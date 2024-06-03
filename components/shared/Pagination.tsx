44ea638a895533cb32cb4f891f459832744dab18
Jozef S <franck.vukelic@gmail.com>
2024-06-03 18:19:57 +0700

    Add: Collection/Card/Delete/Pagination components

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+//
+import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "../ui/button";
+import { formUrlQuery } from "@/lib/utils";
 
-const Pagination = () => {
+// Type definitions for props
+type PaginationProps = {
+  page: number | string;
+  totalPages: number;
+  urlParamName?: string;
+};
+
+//!
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === "next" ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || "page",
+      value: pageValue.toString(),
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
-    <div>Pagination</div>
-  )
-}
+    <div className="flex gap-2">
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick("prev")}
+        disabled={Number(page) <= 1}>
+        Previous
+      </Button>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick("next")}
+        disabled={Number(page) >= totalPages}>
+        Next
+      </Button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;

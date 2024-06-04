e25256af4128cecff4dec8b7f07166b7868c07c6
Jozef S <franck.vukelic@gmail.com>
2024-06-04 15:03:19 +0700

    Fix: card css

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -15,9 +15,9 @@ type CardProps = {
 
 //!
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  // Is the current user the creator of the event ?
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
-
   const isEventCreator = userId === event.organizer._id.toString();
 
   return (
@@ -28,7 +28,6 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
       />
       {/* IS EVENT CREATOR ... */}
-
       {isEventCreator && !hidePrice && (
         <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
           <Link href={`/events/${event._id}/update`}>
@@ -39,12 +38,13 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
               height={20}
             />
           </Link>
-
           <DeleteConfirmation eventId={event._id} />
         </div>
       )}
 
-      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
+      <Link
+        href={`/events/${event._id}`}
+        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         {!hidePrice && (
           <div className="flex gap-2">
             <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
@@ -60,11 +60,9 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           {formatDateTime(event.startDateTime).dateTime}
         </p>
 
-        <Link href={`/events/${event._id}`}>
-          <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
-            {event.title}
-          </p>
-        </Link>
+        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          {event.title}
+        </p>
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
@@ -83,7 +81,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             </Link>
           )}
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
